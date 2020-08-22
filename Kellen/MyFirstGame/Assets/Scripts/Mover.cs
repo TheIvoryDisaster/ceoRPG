5df8d8ac385f5397a1f2b47847742ceb00a4c627
Kellen Kincaid <kellenceo@gmail.com>
2020-08-22 15:34:14 -0400

    New scene with trees and houses and point-and-click movement capabilities and follow-camera capabilities

---
+++ Kellen/MyFirstGame/Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    // Udemy course movement
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+        
+    }
+
+    // For ray-casting.
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+            Debug.DrawRay(ray.origin, ray.direction * 100);
+        }
+    }
+}
