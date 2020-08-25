3d1ac9f9766e33d5bfcfa73afe031526ed7c610c
Matt Warren <themattwarren@gmail.com>
2020-08-25 19:42:01 -0400

    - blend trees

--- Matt/Better Than Kellen's RPG/Assets/Scripts/Mover.cs
+++ Matt/Better Than Kellen's RPG/Assets/Scripts/Mover.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
+    private NavMeshAgent nav;
+    private Animator ant;
+    private Vector3 velocity;
     [SerializeField] Transform target;
 
     // Udemy course movement
@@ -12,10 +16,23 @@ public class Mover : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("moving");
             MoveToCursor();
         }
-
         
+
+    }
+
+    private void UpdateAnimator()
+    {
+        velocity = nav.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        
+    }
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // For ray-casting.
@@ -27,7 +44,7 @@ public class Mover : MonoBehaviour
 
         if (hasHit)
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            nav.destination = hit.point;
             Debug.DrawRay(ray.origin, ray.direction * 100);
         }
     }
